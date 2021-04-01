0ae4b539a3c5e4edb6930a70408c7d661e78fcf3
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2021-04-01 18:55:39 +0530

    Upgrade to latest version

---
+++ Samples/Payments/Payments/authorization-with-tms-token-bypassing-network-token.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var cybersourceRestApi = require('cybersource-rest-client');
+var path = require('path');
+var filePath = path.resolve('Data/Configuration.js');
+var configuration = require(filePath);
+
+function authorization_with_tms_token_bypassing_network_token(callback) {
+	try {
+		var configObject = new configuration();
+		var apiClient = new cybersourceRestApi.ApiClient();
+		var requestObj = new cybersourceRestApi.CreatePaymentRequest();
+
+		var clientReferenceInformation = new cybersourceRestApi.Ptsv2paymentsClientReferenceInformation();
+		clientReferenceInformation.code = 'TC50171_3';
+		requestObj.clientReferenceInformation = clientReferenceInformation;
+
+		var paymentInformation = new cybersourceRestApi.Ptsv2paymentsPaymentInformation();
+		var paymentInformationInstrumentIdentifier = new cybersourceRestApi.Ptsv2paymentsPaymentInformationInstrumentIdentifier();
+		paymentInformationInstrumentIdentifier.id = '7010000000016241111';
+		paymentInformation.instrumentIdentifier = paymentInformationInstrumentIdentifier;
+
+		requestObj.paymentInformation = paymentInformation;
+
+		var orderInformation = new cybersourceRestApi.Ptsv2paymentsOrderInformation();
+		var orderInformationAmountDetails = new cybersourceRestApi.Ptsv2paymentsOrderInformationAmountDetails();
+		orderInformationAmountDetails.totalAmount = '102.21';
+		orderInformationAmountDetails.currency = 'USD';
+		orderInformation.amountDetails = orderInformationAmountDetails;
+
+		requestObj.orderInformation = orderInformation;
+
+		var tokenInformation = new cybersourceRestApi.Ptsv2paymentsTokenInformation();
+		tokenInformation.networkTokenOption = 'ignore';
+		requestObj.tokenInformation = tokenInformation;
+
+
+		var instance = new cybersourceRestApi.PaymentsApi(configObject, apiClient);
+
+		instance.createPayment( requestObj, function (error, data, response) {
+			if(error) {
+				console.log('\nError : ' + JSON.stringify(error));
+			}
+			else if (data) {
+				console.log('\nData : ' + JSON.stringify(data));
+			}
+
+			console.log('\nResponse : ' + JSON.stringify(response));
+			console.log('\nResponse Code of Process a Payment : ' + JSON.stringify(response['status']));
+			callback(error, data, response);
+		});
+	}
+	catch (error) {
+		console.log('\nException on calling the API : ' + error);
+	}
+}
+if (require.main === module) {	
+		authorization_with_tms_token_bypassing_network_token(function () {
+		console.log('\nCreatePayment end.');
+	});
+}
+module.exports.authorization_with_tms_token_bypassing_network_token = authorization_with_tms_token_bypassing_network_token;
