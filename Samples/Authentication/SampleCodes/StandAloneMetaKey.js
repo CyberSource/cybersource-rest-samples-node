085be68d79796fc10072168d1649a3f8599b3c06
snavinch <snavinch@visa.com>
2021-04-23 12:42:34 +0530

    + StandAloneMetaKey Sample code

---
+++ Samples/Authentication/SampleCodes/StandAloneMetaKey.js
@@ -0,0 +1,115 @@
+'use strict';
+
+var cybersourceRestApi = require('cybersource-rest-client');
+
+
+// common parameters
+const AuthenticationType = 'http_signature';
+const RunEnvironment = 'cybersource.environment.SANDBOX';
+const MerchantId = 'merchannt998';
+
+// http_signature parameters
+const MerchantKeyId = '7a7f20a6-1f7f-4780-9a79-3ff8acf7cc83';
+const MerchantSecretKey = 'X+ynT3aTCD643tZgnEnnLD+sd5pplDGMw5vG5klPvVY=';
+
+//meta key parameters
+const UseMetaKey = true;
+const PortfolioID = 'developercp';
+
+function getConfiguration() {
+
+	var configObj = {
+		'authenticationType': AuthenticationType,	
+		'runEnvironment': RunEnvironment,
+
+		'merchantID': MerchantId,
+		'merchantKeyId': MerchantKeyId,
+		'merchantsecretKey': MerchantSecretKey,
+
+		'useMetaKey': UseMetaKey,
+		'portfolioID': PortfolioID
+	};
+	return configObj;
+
+}
+
+
+function standaloneMetaKey(callback)
+{
+    simple_payments_using_meta_key(callback, false)
+}
+
+function simple_payments_using_meta_key(callback, enable_capture) {
+	try {
+		var configObject = getConfiguration();
+		var apiClient = new cybersourceRestApi.ApiClient();
+		var requestObj = new cybersourceRestApi.CreatePaymentRequest();
+
+		var clientReferenceInformation = new cybersourceRestApi.Ptsv2paymentsClientReferenceInformation();
+		clientReferenceInformation.code = 'TC50171_3';
+		requestObj.clientReferenceInformation = clientReferenceInformation;
+
+		var processingInformation = new cybersourceRestApi.Ptsv2paymentsProcessingInformation();
+		processingInformation.capture = false;
+		if (enable_capture === true) {
+			processingInformation.capture = true;
+		}
+
+		requestObj.processingInformation = processingInformation;
+
+		var paymentInformation = new cybersourceRestApi.Ptsv2paymentsPaymentInformation();
+		var paymentInformationCard = new cybersourceRestApi.Ptsv2paymentsPaymentInformationCard();
+		paymentInformationCard.number = '4111111111111111';
+		paymentInformationCard.expirationMonth = '12';
+		paymentInformationCard.expirationYear = '2031';
+		paymentInformation.card = paymentInformationCard;
+
+		requestObj.paymentInformation = paymentInformation;
+
+		var orderInformation = new cybersourceRestApi.Ptsv2paymentsOrderInformation();
+		var orderInformationAmountDetails = new cybersourceRestApi.Ptsv2paymentsOrderInformationAmountDetails();
+		orderInformationAmountDetails.totalAmount = '50.00';
+		orderInformationAmountDetails.currency = 'USD';
+		orderInformation.amountDetails = orderInformationAmountDetails;
+
+		var orderInformationBillTo = new cybersourceRestApi.Ptsv2paymentsOrderInformationBillTo();
+		orderInformationBillTo.firstName = 'John';
+		orderInformationBillTo.lastName = 'Doe';
+		orderInformationBillTo.address1 = '1 Market St';
+		orderInformationBillTo.locality = 'san francisco';
+		orderInformationBillTo.administrativeArea = 'CA';
+		orderInformationBillTo.postalCode = '94105';
+		orderInformationBillTo.country = 'US';
+		orderInformationBillTo.email = 'test@cybs.com';
+		orderInformationBillTo.phoneNumber = '4158880000';
+		orderInformation.billTo = orderInformationBillTo;
+
+		requestObj.orderInformation = orderInformation;
+
+
+		var instance = new cybersourceRestApi.PaymentsApi(configObject, apiClient);
+
+		instance.createPayment(requestObj, function (error, data, response) {
+			if (error) {
+				console.log('\nError : ' + JSON.stringify(error));
+			}
+			else if (data) {
+				console.log('\nData : ' + JSON.stringify(data));
+			}
+
+			console.log('\nResponse : ' + JSON.stringify(response));
+			console.log('\nResponse Code of Process a Payment : ' + JSON.stringify(response['status']));
+			callback(error, data, response);
+		});
+	}
+	catch (error) {
+		console.log('\nException on calling the API : ' + error);
+	}
+}
+
+if (require.main === module) {
+	standaloneMetaKey(function () {
+		console.log('\nStandAloneMetaKey end.');
+	}, false);
+}
+module.exports.standaloneMetaKey = standaloneMetaKey;
