ba9758ac86ef00e210ae01a93f79fa372cc4bfbc
sachshet <sachshet@visa.com>
2023-04-04 10:02:41 +0530

    mar23 release updates

---
+++ Samples/UnifiedCheckout/generate-unified-checkout-capture-context.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var cybersourceRestApi = require('cybersource-rest-client');
+var path = require('path');
+var filePath = path.resolve('Data/Configuration.js');
+var configuration = require(filePath);
+
+function generate_unified_checkout_capture_context(callback) {
+	try {
+		var configObject = new configuration();
+		var apiClient = new cybersourceRestApi.ApiClient();
+		var requestObj = new cybersourceRestApi.GenerateUnifiedCheckoutCaptureContextRequest();
+
+		var captureMandate = new cybersourceRestApi.Upv1capturecontextsCaptureMandate();
+		captureMandate.billingType = 'FULL';
+		captureMandate.requestEmail = true;
+		captureMandate.requestPhone = true;
+        captureMandate.requestShipping = true;
+        captureMandate.showAcceptedNetworkIcons = true;
+        captureMandate.shipToCountries = ["US", "GB"];
+
+		requestObj.captureMandate = captureMandate;
+
+		var orderInformation = new cybersourceRestApi.Upv1capturecontextsOrderInformation();
+		var orderInformationAmountDetails = new cybersourceRestApi.Upv1capturecontextsOrderInformationAmountDetails();
+		orderInformationAmountDetails.totalAmount = '50.00';
+		orderInformationAmountDetails.currency = 'USD';
+		orderInformation.amountDetails = orderInformationAmountDetails;
+
+		requestObj.orderInformation = orderInformation;
+
+        requestObj.locale = 'en_US';
+        requestObj.country = 'US';
+        requestObj.clientVersion = '0.11';
+        requestObj.targetOrigins = ["https://the-up-demo.appspot.com"];
+        
+        requestObj.allowedCardNetworks = ["VISA", "MASTERCARD", "AMEX"]
+        requestObj.allowedPaymentTypes = ["PANENTRY", "SRC"];
+
+
+		var instance = new cybersourceRestApi.UnifiedCheckoutCaptureContextApi(configObject, apiClient);
+
+		instance.generateUnifiedCheckoutCaptureContext(requestObj, function (error, data, response) {
+			if (error) {
+				console.log('\nError : ' + JSON.stringify(error));
+			}
+			else if (data) {
+				console.log('\nData : ' + JSON.stringify(data));
+			}
+
+			console.log('\nResponse : ' + JSON.stringify(response));
+			console.log('\nResponse Code of Process a Payment : ' + JSON.stringify(response['status']));
+			callback(error, data, response);
+		});
+	}
+	catch (error) {
+		console.log('\nException on calling the API : ' + error);
+	}
+}
+if (require.main === module) {
+	generate_unified_checkout_capture_context(function () {
+		console.log('\nGenerate Unified Checkout Capture Context end.');
+	});
+}
+module.exports.generate_unified_checkout_capture_context = generate_unified_checkout_capture_context;
