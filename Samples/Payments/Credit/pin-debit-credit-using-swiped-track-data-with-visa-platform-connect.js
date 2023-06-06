d790d0f95ae29c3ccf3f24a3c186b1792c505a84
Shetty <sachshet@visa.com>
2023-06-06 17:25:27 +0530

    pin-debit samples

---
+++ Samples/Payments/Credit/pin-debit-credit-using-swiped-track-data-with-visa-platform-connect.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var cybersourceRestApi = require('cybersource-rest-client');
+var path = require('path');
+var filePath = path.resolve('Data/AlternativeConfiguration.js');
+var configuration = require(filePath);
+
+function pin_debit_credit_using_swiped_track_data_with_visa_platform_connect(callback) {
+	try {
+		var configObject = new configuration();
+		var apiClient = new cybersourceRestApi.ApiClient();
+		var requestObj = new cybersourceRestApi.CreateCreditRequest();
+
+		var clientReferenceInformation = new cybersourceRestApi.Ptsv2paymentsClientReferenceInformation();
+		clientReferenceInformation.code = '2.2 Credit';
+		requestObj.clientReferenceInformation = clientReferenceInformation;
+
+		var processingInformation = new cybersourceRestApi.Ptsv2creditsProcessingInformation();
+		processingInformation.commerceIndicator = 'retail';
+		requestObj.processingInformation = processingInformation;
+
+		var paymentInformation = new cybersourceRestApi.Ptsv2paymentsidrefundsPaymentInformation();
+		var paymentInformationPaymentType = new cybersourceRestApi.Ptsv2paymentsidrefundsPaymentInformationPaymentType();
+		paymentInformationPaymentType.name = 'CARD';
+		paymentInformationPaymentType.subTypeName = 'DEBIT';
+		paymentInformation.paymentType = paymentInformationPaymentType;
+
+		requestObj.paymentInformation = paymentInformation;
+
+		var orderInformation = new cybersourceRestApi.Ptsv2paymentsidrefundsOrderInformation();
+		var orderInformationAmountDetails = new cybersourceRestApi.Ptsv2paymentsidcapturesOrderInformationAmountDetails();
+		orderInformationAmountDetails.totalAmount = '202.00';
+		orderInformationAmountDetails.currency = 'USD';
+		orderInformation.amountDetails = orderInformationAmountDetails;
+
+		requestObj.orderInformation = orderInformation;
+
+		var merchantInformation = new cybersourceRestApi.Ptsv2paymentsidrefundsMerchantInformation();
+		requestObj.merchantInformation = merchantInformation;
+
+		var pointOfSaleInformation = new cybersourceRestApi.Ptsv2paymentsPointOfSaleInformation();
+		pointOfSaleInformation.entryMode = 'swiped';
+		pointOfSaleInformation.trackData = '%B4111111111111111^JONES/JONES ^3112101976110000868000000?;4111111111111111=16121019761186800000?';
+		requestObj.pointOfSaleInformation = pointOfSaleInformation;
+
+
+		var instance = new cybersourceRestApi.CreditApi(configObject, apiClient);
+
+		instance.createCredit( requestObj, function (error, data, response) {
+			if(error) {
+				console.log('\nError : ' + JSON.stringify(error));
+			}
+			else if (data) {
+				console.log('\nData : ' + JSON.stringify(data));
+			}
+
+			console.log('\nResponse : ' + JSON.stringify(response));
+			console.log('\nResponse Code of Process a Credit : ' + JSON.stringify(response['status']));
+			var status = response['status'];
+			write_log_audit(status);
+			callback(error, data, response);
+		});
+	}
+	catch (error) {
+		console.log('\nException on calling the API : ' + error);
+	}
+}
+
+function write_log_audit(status) {
+	var filename = path.basename(__filename).split(".")[0];
+	console.log(`[Sample Code Testing] [${filename}] ${status}`);
+}
+
+if (require.main === module) {	
+		pin_debit_credit_using_swiped_track_data_with_visa_platform_connect(function () {
+		console.log('\nCreateCredit end.');
+	});
+}
+module.exports.pin_debit_credit_using_swiped_track_data_with_visa_platform_connect = pin_debit_credit_using_swiped_track_data_with_visa_platform_connect;
