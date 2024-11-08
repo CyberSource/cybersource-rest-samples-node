d073f97400c2d9368030fe1171a946e534163571
gaubansa <gaubansa@visa.com>
2024-11-08 18:14:33 +0530

    adding MLE samples code

---
+++ Samples/MLEFeature/PaymentsWithMLEConteolFromMapConfigOnly.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var cybersourceRestApi = require('cybersource-rest-client');
+var path = require('path');
+var filePath = path.resolve('Data/ConfigurationWithMLE.js');
+var {MLEConfiguration3} = require(filePath);
+
+function simple_authorization_internet_with_Map_Control_MLE_True(callback, enable_capture) {
+	try {
+        //useMLEGlobally=false in config, but mapToControlMLEonAPI has createPayment=true, so MLE will be enable only for createPayment function/method.
+		var configObject = new MLEConfiguration3();
+
+		var apiClient = new cybersourceRestApi.ApiClient();
+		var requestObj = new cybersourceRestApi.CreatePaymentRequest();
+
+		var clientReferenceInformation = new cybersourceRestApi.Ptsv2paymentsClientReferenceInformation();
+		clientReferenceInformation.code = 'TC50171_3';
+		requestObj.clientReferenceInformation = clientReferenceInformation;
+
+		var processingInformation = new cybersourceRestApi.Ptsv2paymentsProcessingInformation();
+		processingInformation.capture = false;
+		if (enable_capture === true) {
+			processingInformation.capture = true;
+		}
+
+		requestObj.processingInformation = processingInformation;
+
+		var paymentInformation = new cybersourceRestApi.Ptsv2paymentsPaymentInformation();
+		var paymentInformationCard = new cybersourceRestApi.Ptsv2paymentsPaymentInformationCard();
+		paymentInformationCard.number = '4111111111111111';
+		paymentInformationCard.expirationMonth = '12';
+		paymentInformationCard.expirationYear = '2031';
+		paymentInformation.card = paymentInformationCard;
+
+		requestObj.paymentInformation = paymentInformation;
+
+		var orderInformation = new cybersourceRestApi.Ptsv2paymentsOrderInformation();
+		var orderInformationAmountDetails = new cybersourceRestApi.Ptsv2paymentsOrderInformationAmountDetails();
+		orderInformationAmountDetails.totalAmount = '50.00';
+		orderInformationAmountDetails.currency = 'USD';
+		orderInformation.amountDetails = orderInformationAmountDetails;
+
+		var orderInformationBillTo = new cybersourceRestApi.Ptsv2paymentsOrderInformationBillTo();
+		orderInformationBillTo.firstName = 'John';
+		orderInformationBillTo.lastName = 'Doe';
+		orderInformationBillTo.address1 = '1 Market St';
+		orderInformationBillTo.locality = 'san francisco';
+		orderInformationBillTo.administrativeArea = 'CA';
+		orderInformationBillTo.postalCode = '94105';
+		orderInformationBillTo.country = 'US';
+		orderInformationBillTo.email = 'test@cybs.com';
+		orderInformationBillTo.phoneNumber = '4158880000';
+		orderInformation.billTo = orderInformationBillTo;
+
+		requestObj.orderInformation = orderInformation;
+
+
+		var instance = new cybersourceRestApi.PaymentsApi(configObject, apiClient);
+
+		instance.createPayment(requestObj, function (error, data, response) {
+			if (error) {
+				console.log('\nError : ' + JSON.stringify(error));
+			}
+			else if (data) {
+				console.log('\nData : ' + JSON.stringify(data));
+			}
+
+			console.log('\nResponse : ' + JSON.stringify(response));
+			console.log('\nResponse Code of Process a Payment with mapToControlMLEonAPI true : ' + JSON.stringify(response['status']));
+			var status = response['status'];
+			write_log_audit(status);
+			callback(error, data, response);
+		});
+	}
+	catch (error) {
+		console.log('\nException on calling the API : ' + error);
+	}
+}
+
+function write_log_audit(status) {
+	var filename = path.basename(__filename).split(".")[0];
+	console.log(`[Sample Code Testing] [${filename}] ${status}`);
+}
+
+if (require.main === module) {
+	simple_authorization_internet_with_Map_Control_MLE_True(function () {
+		console.log('\nCreatePayment with with mapToControlMLEonAPI has true end.');
+	});
+}
+module.exports.simple_authorization_internet_with_Map_Control_MLE_True = simple_authorization_internet_with_Map_Control_MLE_True;
