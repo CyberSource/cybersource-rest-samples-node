38dfb812e6aada1986e80ce3c02f9efcc6cba560
mahmishr <mahmishr@visa.com>
2025-02-07 12:50:12 +0530

    "samples"

---
+++ Samples/FlexMicroform/generate-capture-context-accept-card.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var cybersourceRestApi = require('cybersource-rest-client');
+var path = require('path');
+var filePath = path.resolve('Data/Configuration.js');
+var configuration = require(filePath);
+
+function generate_capture_context_accept_card(callback) {
+    try {
+        var configObject = new configuration();
+        var apiClient = new cybersourceRestApi.ApiClient();
+        var requestObj = new cybersourceRestApi.GenerateCaptureContextRequest();
+
+        requestObj.clientVersion = 'v2';
+
+        var targetOrigins = new Array();
+        targetOrigins.push("https://www.test.com");
+        requestObj.targetOrigins = targetOrigins;
+
+
+        var allowedCardNetworks = new Array();
+        allowedCardNetworks.push("VISA");
+        allowedCardNetworks.push("MASTERCARD");
+        allowedCardNetworks.push("AMEX");
+        allowedCardNetworks.push("CARNET");
+        allowedCardNetworks.push("CARTESBANCAIRES");
+        allowedCardNetworks.push("CUP");
+        allowedCardNetworks.push("DINERSCLUB");
+        allowedCardNetworks.push("DISCOVER");
+        allowedCardNetworks.push("EFTPOS");
+        allowedCardNetworks.push("ELO");
+        allowedCardNetworks.push("JCB");
+        allowedCardNetworks.push("JCREW");
+        allowedCardNetworks.push("MADA");
+        allowedCardNetworks.push("MAESTRO");
+        allowedCardNetworks.push("MEEZA");
+        requestObj.allowedCardNetworks = allowedCardNetworks;
+
+        var allowedPaymentTypes = new Array();
+        allowedPaymentTypes.push("CARD");
+        requestObj.allowedPaymentTypes = allowedPaymentTypes;
+
+
+        var instance = new cybersourceRestApi.MicroformIntegrationApi(configObject, apiClient);
+
+        instance.generateCaptureContext(requestObj, function (error, data, response) {
+            if (error) {
+                console.log('\nError : ' + JSON.stringify(error));
+            }
+            else if (data) {
+                console.log('\nData : ' + JSON.stringify(data));
+            }
+
+            console.log('\nResponse : ' + JSON.stringify(response));
+            console.log('\nResponse Code of Process a Payment : ' + JSON.stringify(response['status']));
+            callback(error, data, response);
+        });
+    }
+    catch (error) {
+        console.log('\nException on calling the API : ' + error);
+    }
+}
+
+function write_log_audit(status) {
+    var filename = path.basename(__filename).split(".")[0];
+    console.log(`[Sample Code Testing] [${filename}] ${status}`);
+}
+
+if (require.main === module) {
+    generate_capture_context_accept_card(function () {
+        console.log('\nGenerateCaptureContext end.');
+    });
+}
+
+module.exports.generate_capture_context_accept_card = generate_capture_context_accept_card;
